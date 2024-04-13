12ceaed6ab8b503a2eeb0ed85c22e2d5b779f0c0
Loretto Herrera <loretto.herrera.sossa@ciisa.cl>
2024-04-13 00:51:56 -0400

    Se corrige codigo, se agregan estilos y se incluyen nuevas imagenes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const tabs = document.querySelectorAll('.tabs li a');
 const tabContent = document.querySelectorAll('.tab-pane');
 
+
 tabs.forEach((tab, index) => {
     tab.addEventListener('click', (e) => {
         e.preventDefault();
@@ -20,3 +21,15 @@ function hideAllTabs() {
 function showTabContent(index) {
     tabContent[index].style.display = 'block';
 }
+
+// Cambiar la imagen cada 3 segundos
+const images = document.querySelectorAll('.carousel img');
+let index = 0;
+
+function changeImage() {
+    images[index].style.transform = 'translateX(-100%)';
+    index = (index + 1) % images.length;
+    images[index].style.transform = 'translateX(0)';
+}
+
+setInterval(changeImage, 2000);
